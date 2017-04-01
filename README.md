02822ce5e5a1cc9d9d8ad07a9da3aa79afbeec8a
Jonas Wagner <boom@confuflux.com>
2017-03-31 22:18:34 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # twilio-sms-to-mail
-quick n' dirty twilio text (sms) to mail python app
+quick n' dirty twilio text (sms) to mail python app using postmark app
+
+forwards all incoming text messages to your twilio phone number to a given email address
+
+# 1. prep
+
+1. get a https://www.twilio.com/ account and get a phone number ($1/month + per [sms fee](https://www.twilio.com/sms/pricing))
+2. get a [https://postmarkapp.com/](https://postmarkapp.com/) account, you'll get 25,000 credits for free. That's about... 25,000 emails.
+
+# 2. config
+
+* retrieve your PostmarkApp API key and add it to `application.py` as 
+`token`
+* change the `toAddress` and `fromAddress` parts in `applicaiton.py` to fit your needs
+
+# 3. run it
+
+It's 2017, so run it in a docker container:
+
+```
+docker build -t twilio-sms2mail .
+docker run -d -p5000:5000 --name twilio twilio-sms2mail
+```
+
+per default the flask app will listen on port 5000, you can either change the port through docker or use a http reverse proxy like nginx or caddy
---
+++ application.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import requests
+
+"""
+configure your PostmarkApp token, to and from address here
+the from address needs to be verified with postmark, otherwise it'll fail
+"""
+token = 'your-postmark-token-here'
+toAddress = 'your-to-email@example.com'
+fromAddress = 'your-from-address-email@example.com'
+
+
+url = 'https://api.postmarkapp.com/email'
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def receive():
+    if request.method == 'POST':
+        msgFrom = request.form["From"]
+        msgTo = request.form["To"]
+        msgBody = request.form["Body"]
+
+        subject = "New SMS from %s" % (msgFrom)
+
+        payload = {'From': fromAddress, 'To': toAddress, 'Subject': subject, 'Textbody': msgBody}
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'X-Postmark-Server-Token': token}
+        req = requests.post(url, json=payload, headers=headers)
+
+        if req.status_code == 200:
+            return 'thx', 200
+        else:
+            return req.text
+
+    else:
+        return '', 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
